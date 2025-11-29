92f394c081b440e960daef799a94ff0d1cf46d8c
SergeiCH <cheremushnikoff@yandex.ru>
2025-11-29 21:17:27 +0800

    Применил базовые миграции; Зарегистрировал модель пользователя в админке

--- users/models.py
+++ users/models.py
@@ -24,6 +24,11 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        db_table = 'users_users'
+
     def __str__(self):
         return self.email
 
