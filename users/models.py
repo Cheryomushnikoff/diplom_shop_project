2c068cd3c691b79789b99f65cad3a99558860c77
SergeiCH <cheremushnikoff@yandex.ru>
2025-11-29 16:45:23 +0800

    Создал(models.py, managers.py) и подключил(settings.py) кастомную модель пользователя(отсутствие поля username, использование email в качестве логина)

--- users/models.py
+++ users/models.py
@@ -1,4 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from .managers import UserManager
 
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, unique=True, blank=True, null=True)
+
+    address = models.CharField(max_length=500, blank=True)
+
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    second_name = models.CharField(max_length=200, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    subscribed_to_newsletter = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
 
 
 
