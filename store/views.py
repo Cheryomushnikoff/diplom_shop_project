dcde260493e8ce202f338edc04756e44f26d384e
SergeiCH <cheremushnikoff@yandex.ru>
2025-12-07 16:39:31 +0800

    Создал путь, вьюшку, шаблон, и ссылку в базовом шаблоне для отображения каталога товаров

--- store/views.py
+++ store/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from .models import Category, Product
+
 def main_page_view(request):
     return render(request, 'store/main_page.html')
+
+def product_list_view(request):
+    product_list = Product.objects.all()
+
+    return render(request, 'store/product_list.html', {'product_list': product_list})
