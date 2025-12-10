b04a4460a36ff2c5fd53e195dd6d0ba435f86e03
SergeiCH <cheremushnikoff@yandex.ru>
2025-12-11 00:16:00 +0800

    Создал представление и шаблон авторизации и деавторизации пользователя

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthForm
 
 def register_view(request):
     form = CustomUserCreationForm()
@@ -13,3 +14,21 @@ def register_view(request):
             return redirect('store:main_page')
 
     return render(request,'users/register.html', {'form': form.render('users/form_snippet.html')})
+
+
+def login_view(request):
+    form = CustomAuthForm()
+
+    if request.method == 'POST':
+        form = CustomAuthForm(request.POST)
+
+        if form.is_valid():
+            user = form.cleaned_data['user']
+            login(request, user)
+            return redirect('store:main_page')
+    return render(request, 'users/login.html', context={'form': form.render('users/form_snippet.html')})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('store:main_page')
