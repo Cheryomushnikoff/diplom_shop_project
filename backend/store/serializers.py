937f15101ec115ea5c7be37708bd8654305b6f7f
SergeiCH <cheremushnikoff@yandex.ru>
2026-01-18 02:57:42 +0800

    добавил сериализатор для элементов корзины

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, CartItem
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,6 +16,12 @@ class ProductSerializer(serializers.ModelSerializer):
             'created_at' ,
         ]
 
-    def create(self, validated_data):
-        return Product.objects.create(**validated_data)
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), write_only=True, source='product')
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'product_id', 'quantity']
+
 
