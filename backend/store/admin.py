c420cf11fe88379c7917f267dc2aa83582b3e250
SergeiCH <cheremushnikoff@yandex.ru>
2026-01-30 11:48:27 +0800

    зарегал в админке заказы для возможности менять статусы заказа

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Order, OrderItem
 
 admin.site.register(Category)
 admin.site.register(Product)
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ("product", "price", "quantity")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created_at",
+        "status",
+        "total_price",
+        "email"
+    )
+    list_filter = ("status", "created_at")
+    search_fields = ("id", "email", "phone")
+    inlines = [OrderItemInline]
+    readonly_fields = ("total_price", "created_at")
